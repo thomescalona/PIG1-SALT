64c2e0c90f0907243690abe0c8e06a0de22f6e7f
Álvaro <alvgarsan5@alu.edu.gva.es>
2025-02-27 12:42:14 +0100

    Update Bolso.java
    añadir material

--- src/subclase_articulo/subclase_accesorio/Bolso.java
+++ src/subclase_articulo/subclase_accesorio/Bolso.java
@@ -20,15 +20,15 @@ public class Bolso extends Accesorio {
         this.capacidad = capacidad;
     }
 
-    public Bolso(String codigoArticulo, String nombre, float precio, String marca, String descripcion, String imagen, boolean activo, String estilo, boolean esPersonalizado, String tipoCierre, int capacidad) {
 
-        super(codigoArticulo, nombre, precio, marca, descripcion, imagen, activo, estilo, esPersonalizado);
+
+    public Bolso(String codigoArticulo, String nombre, float precio, String marca, String descripcion, String imagen, Material material, boolean activo, String estilo, boolean esPersonalizado, String tipoCierre, int capacidad) {
+        super(codigoArticulo, nombre, precio, marca, descripcion, imagen, material, activo, estilo, esPersonalizado);
         this.tipoCierre = tipoCierre;
         this.capacidad = capacidad;
-
-
-
     }
+
+
 
     public void mostrarDetalles(){
         System.out.println(getNombre() + " tengo un cierre" + getTipoCierre() + " y capacidad:" + getCapacidad());
