90d75459b7112c953d3783e56bf259d79a3c302a
Álvaro <alvgarsan5@alu.edu.gva.es>
2025-02-27 12:43:33 +0100

    Update Zapatos.java
    actualizacion con Material

--- src/subclase_articulo/subclase_accesorio/Zapatos.java
+++ src/subclase_articulo/subclase_accesorio/Zapatos.java
@@ -5,9 +5,8 @@ public class Zapatos extends Accesorio {
 
     private String tipoSuela;
 
-    public Zapatos(String codigoArticulo, String nombre, float precio, String marca, String descripcion, String imagen, boolean activo, String estilo, boolean esPersonalizado,
-                   int tallaZapatos, String tipoSuela) {
-        super(codigoArticulo, nombre, precio, marca, descripcion, imagen, activo, estilo, esPersonalizado);
+    public Zapatos(String codigoArticulo, String nombre, float precio, String marca, String descripcion, String imagen, Material material, boolean activo, String estilo, boolean esPersonalizado, int tallaZapatos, String tipoSuela) {
+        super(codigoArticulo, nombre, precio, marca, descripcion, imagen, material, activo, estilo, esPersonalizado);
         this.tallaZapatos = tallaZapatos;
         this.tipoSuela = tipoSuela;
     }
@@ -28,7 +27,8 @@ public class Zapatos extends Accesorio {
         this.tipoSuela = tipoSuela;
     }
 
-   public void mostrarDetalles(){
-       System.out.println(getNombre() + "tengo una talla : " + getTallaZapatos() + " y una suela " + getTipoSuela());
-   }
+    public void mostrarDetalles(){
+        System.out.println(getNombre() + "tengo una talla : " + getTallaZapatos() + " y una suela " + getTipoSuela());
+    }
 }
+
